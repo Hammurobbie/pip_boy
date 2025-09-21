7f0c1b875505460a39b6953697c78e10e25b7589
hammurobbie <rthomporter@gmail.com>
2025-09-21 04:21:01 -0400

    at'll do, donkey

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-function playSound(sound_file) {
-  const sound = document.getElementById("clickSound");
-  sound.src = `assets/${sound_file}.mp3`;
-  sound.currentTime = 0;
-  sound.play();
+function playSound(id) {
+  var sound = document.getElementById("sound-" + id);
+  if (sound) {
+    try {
+      sound.currentTime = 0;
+    } catch (e) {}
+    sound.play();
+  }
 }
 
 function showPanel(id) {
   var panels = document.getElementsByClassName("panel");
   var buttons = document.getElementsByTagName("button");
+
   for (var i = 0; i < panels.length; i++) {
     panels[i].className = "panel";
   }
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].className = "";
   }
+
   document.getElementById(id).className = "panel active";
   document.getElementById("button-" + id).className = "clicked";
+
   var file =
     id === "map"
       ? "geiger_counter"
       : id === "radio"
       ? "world_on_fire"
+      : id === "inventory"
+      ? "vats_lock_2"
+      : id === "data"
+      ? "vats_lock_3"
       : "vats_lock";
   playSound(file);
 }
@@ -33,13 +43,9 @@ function flickerWrapper(wrapperId) {
   }, 250);
 }
 
-document.addEventListener(
-  "touchmove",
-  function (e) {
-    e.preventDefault();
-  },
-  { passive: false }
-);
+document.addEventListener("touchmove", function (e) {
+  e.preventDefault();
+});
 
 window.onload = function () {
   flickerWrapper("body");
